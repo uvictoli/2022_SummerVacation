03f5f55d0bc517cf6606371585ecbdaa98bc8683
Yoo SeungLi <69826406+uvictoli@users.noreply.github.com>
2022-07-11 14:04:09 +0900

    220711 데이터 분석

---
+++ study/chapter1/chapter_1_program.R
@@ -0,0 +1,83 @@
+#chapter_1_program.r"
+#20220710-jclee
+
+#install package - #update r version()
+if(!(installed.packages() %in% c("instalr","rstudiopapi")))
+  install.packages("instalr",dependencies=T)
+install.packages(c("instalr","rstudioapi"), dependecncies=T)
+library(installr)
+library(rsudioapi)
+rstCheck<-check.for.updates.R()
+if(rskChek==TRUE)
+  updateR()
+
+# Prepreperation
+getwd()
+setwd("/Users/yooseungli/Downloads/데청캠/오프라인 수업/220711/chapter1")
+installed.packages("rstudioapi",dependencies=T)
+library(rstudioapi)
+curPosition<-rstudioapi::getActiveDocumentContext()$path
+curDir<-dirname(curPosition)
+# curDir
+setwd(curDir)
+
+
+# The Anscsombe Quartet in R
+
+# define the anscombe data frame
+anscombe <- data.frame(
+  x1 = c(10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5),
+  x2 = c(10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5),
+  x3 = c(10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5),
+  x4 = c(8, 8, 8, 8, 8, 8, 8, 19, 8, 8),
+  y1 = c(8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68),
+  y2 = c(9.14, 8.14, 8.74, 8.77, 9.26, 8.1, 6.13, 3.1, 9.13, 7.26, 4.74),
+  y3 = c(7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73),
+  y4 = c(6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.5, 5.56, 7.91, 6.89)
+)
+
+#class(anscombe) # data type 확인
+
+# Export Dataframe to excel
+install.packages("writexl", dependencies = T)
+library(writexl)
+write_xlsx(anscombe, "anscombe.xlsx")
+
+# Show summaries X
+with(anscombe, summary(x1))
+with(anscombe, summary(x2))
+with(anscombe, summary(x3))
+with(anscombe, summary(x4))
+
+# Show summaries Y
+with(anscombe, summary(y1))
+with(anscombe, summary(y2))
+with(anscombe, summary(y3))
+with(anscombe, summary(y4))
+
+# show results from four regression analyses
+with(anscombe, print(summary(lm(y1 ~ x1))))
+with(anscombe, print(summary(lm(y2 ~ x2))))
+with(anscombe, print(summary(lm(y3 ~ x3))))
+with(anscombe, print(summary(lm(y4 ~ x4))))
+
+# place four plots in ine page using standard R graphics
+# ensuring that all have the same scales
+# for horizontal and vertical axes
+pdf(file= "fig_more_anscombe.pdf", width = 8.5, height = 8.5)
+par(mfrow = c(2,2), mar = c(3, 3, 3, 1))
+with(anscombe, plot(x1, y1, xlim = c(2, 20), ylim = c(2, 14),
+                    pch = 19, col = "darkblue", cex = 2, las = 1))
+title("Set I")
+with(anscombe, plot(x2, y2, xlim = c(2, 20), ylim = c(2, 14),
+                    pch = 19, col = "darkblue", cex = 2, las = 1))
+title("Set II")
+with(anscombe, plot(x3, y3, xlim = c(2, 20), ylim = c(2, 14),
+                    pch = 19, col = "darkblue", cex = 2, las = 1))
+title("Set III")
+with(anscombe, plot(x4, y4, xlim = c(2, 20), ylim = c(2, 14),
+                    pch = 19, col = "darkblue", cex = 2, las = 1))
+title("Set IV")
+dev.off()
+
+par(mfrow = c(1, 1), mar = c(5.1, 4.1, 4.1, 2.1)) #return to plotting defaults
